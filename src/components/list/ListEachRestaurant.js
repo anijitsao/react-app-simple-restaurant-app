e8034612cc13e4e0136e980ca201a12ae01e931e
AnijitSau <2anijit@gmail.com>
2018-12-26 17:57:44 +0530

    props are sent as part of Link

--- src/components/list/ListEachRestaurant.js
+++ src/components/list/ListEachRestaurant.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 
 const ListEachRestaurant = (props) => {
 	console.log('Props received', props)
+	let { restaurant } = props.location
 	return (
 		<div className="restaurant-details">
 			<Link to="/"><i className="fa fa-arrow-left"></i></Link>
-			
+			{restaurant.name}
 		</div>
 	);
 }
