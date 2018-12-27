6e90dbbc974e05250d2b05b289ae8ea4b1159d93
AnijitSau <2anijit@gmail.com>
2018-12-27 13:03:30 +0530

    Each restaurant page is improved with css

--- src/components/list/ListEachRestaurant.js
+++ src/components/list/ListEachRestaurant.js
@@ -7,8 +7,24 @@ const ListEachRestaurant = (props) => {
 	let { restaurant } = props.location
 	return (
 		<div className="restaurant-details">
-			<Link to="/"><i className="fa fa-arrow-left"></i></Link>
-			{restaurant.name}
+			<div className="restaurant-photo">
+
+				<div className="restaurant-name-description">
+					<Link to="/"><i className="fa fa-arrow-left"></i></Link>
+					{restaurant.name}
+				</div>
+				<div className="restaurant-rating">{restaurant.rating}</div>
+			</div>
+			<div className="restaurant-description">
+				<div className="category-div">
+					<div className="category-title">cuisine</div>
+					<div className="category-description">{restaurant.cuisines.join(', ')}</div>
+				</div>
+				<div className="category-div">
+					<div className="category-title">address</div>
+					<div className="category-description">{restaurant.address}</div>
+				</div>
+			</div>
 		</div>
 	);
 }
